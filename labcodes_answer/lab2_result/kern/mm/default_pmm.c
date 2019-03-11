741baee4a173446a77145dbf82edaf4b229e8ef5
wasabbi <lee.yeb33@gmail.com>
2019-03-11 23:46:11 +0800

    lab2_1

--- labcodes_answer/lab2_result/kern/mm/default_pmm.c
+++ labcodes_answer/lab2_result/kern/mm/default_pmm.c
@@ -181,7 +181,7 @@ default_free_pages(struct Page *base, size_t n) {
     le = list_next(&free_list);
     while (le != &free_list) {
         p = le2page(le, page_link);
-        if (base + base->property <= p) {
+        if (base + base->property <= p) {       //insert by addr order
             assert(base + base->property != p);
             break;
         }
