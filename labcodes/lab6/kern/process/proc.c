34f749c30dd15b01115a0f8aadc4e3dc6d20b699
wasabbi <lee.yeb33@gmail.com>
2019-03-19 10:16:39 +0800

    lab6_improve

--- labcodes/lab6/kern/process/proc.c
+++ labcodes/lab6/kern/process/proc.c
@@ -103,20 +103,44 @@ alloc_proc(void) {
      *       uint32_t flags;                             // Process flag
      *       char name[PROC_NAME_LEN + 1];               // Process name
      */
-     proc->state = PROC_UNINIT;
-     proc->pid = -1;
-     proc->runs = 0;
-     proc->kstack = 0;
-     proc->need_resched = 0;
-     proc->parent = NULL;
-     proc->mm = NULL;
-     memset(&(proc->context), 0, sizeof(struct context));
-     proc->tf = NULL;
-     proc->cr3 = boot_cr3;
-     proc->flags = 0;
-     memset(proc->name, 0, PROC_NAME_LEN);
-     proc->wait_state = 0; 
-     proc->cptr = proc->yptr = proc->optr = NULL;   //lab5
+     //LAB5 2015080062 : (update LAB4 steps)
+    /*
+     * below fields(add in LAB5) in proc_struct need to be initialized	
+     *       uint32_t wait_state;                        // waiting state
+     *       struct proc_struct *cptr, *yptr, *optr;     // relations between processes
+	 */
+     //LAB6 2015080062 : (update LAB5 steps)
+    /*
+     * below fields(add in LAB6) in proc_struct need to be initialized
+     *     struct run_queue *rq;                       // running queue contains Process
+     *     list_entry_t run_link;                      // the entry linked in run queue
+     *     int time_slice;                             // time slice for occupying the CPU
+     *     skew_heap_entry_t lab6_run_pool;            // FOR LAB6 ONLY: the entry in the run pool
+     *     uint32_t lab6_stride;                       // FOR LAB6 ONLY: the current stride of the process
+     *     uint32_t lab6_priority;                     // FOR LAB6 ONLY: the priority of process, set by lab6_set_priority(uint32_t)
+     */
+        proc->state = PROC_UNINIT;
+        proc->pid = -1;
+        proc->runs = 0;
+        proc->kstack = 0;
+        proc->need_resched = 0;
+        proc->parent = NULL;
+        proc->mm = NULL;
+        memset(&(proc->context), 0, sizeof(struct context));
+        proc->tf = NULL;
+        proc->cr3 = boot_cr3;
+        proc->flags = 0;
+        memset(proc->name, 0, PROC_NAME_LEN);
+
+        proc->wait_state = 0; 
+        proc->cptr = proc->yptr = proc->optr = NULL;   //lab5
+        
+        proc->rq = NULL;
+        list_init(&(proc->run_link));   //run_link->prev = run_link, run_link->next = run_link
+        proc->time_slice = 0;
+        proc->lab6_run_pool.left = proc->lab6_run_pool.right = proc->lab6_run_pool.parent = NULL;
+        proc->lab6_stride = 0;
+        proc->lab6_priority = 0;        //lab6
     }
     return proc;
 }
